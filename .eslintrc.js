c3cf93b438bb3b88a14ca098f531ca844f71cdc1
ItsMidnights <michaelcasas13@gmail.com>
2022-03-10 01:27:51 -0600

    feat: eslint, commitlint, husky, lint-staged init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    indent: ["error", 2],
+    linebreakStyle: "off",
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
